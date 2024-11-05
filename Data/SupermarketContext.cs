20f6b775cf678ad57bd908a71ff1c7e75e01062d
LuisMain07 <bdmainguez00@gmail.com>
2024-11-04 22:05:51 -0500

    Agregar archivos de proyecto.

---
+++ Data/SupermarketContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using SuperMarketWeb1.Models;
+
+namespace SuperMarketWeb1.Data
+{
+    public class SupermarketContext : DbContext
+    {
+        public SupermarketContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // DbSet<PayMode> PayModes { get; set; }
+
+        //public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
